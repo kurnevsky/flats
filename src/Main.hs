79bf79cb6a34971a6cd821f634562c6a17f7e675
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-07-05 00:17:14 +0300

    Add some more output.

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,10 @@ main :: IO ()
 main = do
   time <- getCurrentTime
   manager <- newManager tlsManagerSettings
+  putStrLn "Retrieving data..."
   flats <- grab manager
   let csv = encodeDefaultOrderedByName flats
-  BS.writeFile (formatTime defaultTimeLocale "onliner-%Y-%m-%dT%H:%M:%S.csv" time) csv
+      filename = formatTime defaultTimeLocale "onliner-%Y-%m-%dT%H:%M:%S.csv" time
+  putStrLn "Saving data..."
+  BS.writeFile filename csv
+  putStrLn $ "Saved to " ++ filename
